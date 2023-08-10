abbfa5c5e05d3eebe756937ff3058848c6b9c569
ontariodevelopernetwork <admin@ontariodevelopers.com>
2023-08-10 12:07:09 -0400

    update #5

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import os
 os.system("rm -R api")
 os.system("rm index.html")
 os.system("git clone https://github.com/ontariodevelopernetwork/api.git")
-os.system("cp var/www/html/api/index.html /var/www/html")
+os.system("cp /var/www/html/api/index.html /var/www/html")
